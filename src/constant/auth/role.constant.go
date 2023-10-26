d61c1e6550868baf3df2f0663fd903a29557467e
Idhibhat Pankam <marginpankam@gmail.com>
2023-10-26 22:28:32 +0700

    feat: remove ununsed

--- src/constant/auth/role.constant.go
+++ src/constant/auth/role.constant.go
@@ -3,8 +3,6 @@ package auth
 type Role string
 
 const (
-	ADMIN       Role = "admin"
-	EVENT_STAFF      = "event_staff"
-	BAAN_STAFF       = "baan_staff"
-	USER             = "user"
+	ADMIN Role = "admin"
+	USER       = "user"
 )
