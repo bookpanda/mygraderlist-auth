0271279bcb6ad01718ce7a33b43fbf8c09cd2f28
Idhibhat Pankam <marginpankam@gmail.com>
2023-11-18 20:49:59 +0700

    feat: remove log

--- src/database/redis.connection.go
+++ src/database/redis.connection.go
@@ -4,11 +4,9 @@ import (
 	"github.com/bookpanda/mygraderlist-auth/src/config"
 	"github.com/go-redis/redis/v8"
 	"github.com/pkg/errors"
-	"github.com/rs/zerolog/log"
 )
 
 func InitRedisConnect(conf *config.Redis) (cache *redis.Client, err error) {
-	log.Info().Any("conf", conf)
 	cache = redis.NewClient(&redis.Options{
 		Addr:     conf.Host,
 		DB:       0,
