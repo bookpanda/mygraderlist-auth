95fd843c73f4e68989bd0ad54d8addb6a5c77f78
Idhibhat Pankam <marginpankam@gmail.com>
2023-11-18 19:44:08 +0700

    tmp: log

--- src/database/redis.connection.go
+++ src/database/redis.connection.go
@@ -4,9 +4,11 @@ import (
 	"github.com/bookpanda/mygraderlist-auth/src/config"
 	"github.com/go-redis/redis/v8"
 	"github.com/pkg/errors"
+	"github.com/rs/zerolog/log"
 )
 
 func InitRedisConnect(conf *config.Redis) (cache *redis.Client, err error) {
+	log.Info().Any("conf", conf)
 	cache = redis.NewClient(&redis.Options{
 		Addr:     conf.Host,
 		DB:       0,
